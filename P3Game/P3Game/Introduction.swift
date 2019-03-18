df46c2637d9acef1a978fc17d32dee29a94bf939
Madaragog <cedric.dorez@gmail.com>
2019-03-18 17:45:35 +0100

    creation commandLineTools project

---
+++ P3Game/P3Game/Introduction.swift
@@ -0,0 +1,15 @@
+//
+//  Introduction.swift
+//  P3Game
+//
+//  Created by Cedric on 18/03/2019.
+//  Copyright © 2019 Cedric. All rights reserved.
+//
+
+import Foundation
+
+class Introduction {
+    func start() {
+        print("Hi and welcome to P3Game ")
+    }
+}
---
+++ P3Game/P3Game/main.swift
@@ -0,0 +1,2 @@
+let intro = Introduction()
+intro.start()
