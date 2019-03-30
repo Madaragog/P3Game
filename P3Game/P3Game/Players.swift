147a3d497acb3313bd3565be856cf9a9697ba1ad
Madaragog <cedric.dorez@gmail.com>
2019-03-30 19:30:53 +0100

    debugging "player1TeamCreation()" and "player2TeamCreation()"

--- P3Game/P3Game/Players.swift
+++ P3Game/P3Game/Players.swift
@@ -52,7 +52,7 @@ class PlayerManager {
             + "\n The Wizard (life point: 70, weapon: Magic Stick, Healing capacity: 10)"
             + "\n The Colossus (life point: 120, weapon: Mace, weapon damage: 16)"
             + "\n The Dwarf (life point: 80, weapon: Axe, weapon damage: 15)"
-            + "\n The Wizard can omly choose a comrade to heal"
+            + "\n The Wizard can only choose a comrade to heal"
             + "\n ************************************************************"
             + "\n \(player1!.playerName) please choose your Characters and name them"
             + "\n To choose press 1 for The Fighter, 2 for The Wizard"
@@ -72,17 +72,31 @@ class PlayerManager {
             case "1":
                 print("Choose your Fighter's name")
                 player1?.fighter = createFighterPlayer()
+                print("\(player1!.playerName) when you're done with your 3 characters, press c to continue")
+                player1TeamCreation()
             case "2":
                 print("Choose your Wizard's name")
                 player1?.wizard = createWizardPlayer()
+                print("\(player1!.playerName) when you're done with your 3 characters, press c to continue")
+                player1TeamCreation()
             case "3":
                 print("Choose your Colossus's name")
                 player1?.colossus = createColossusPlayer()
+                print("\(player1!.playerName) when you're done with your 3 characters, press c to continue")
+                player1TeamCreation()
             case "4":
-                print("Choose your Dwarf's name")
-                player1?.dwarf = createDwarfPlayer()
+                print("Choose your Fighter's name")
+                player1?.fighter = createFighterPlayer()
+                print("\(player1!.playerName) when you're done with your 3 characters, press c to continue")
+                player1TeamCreation()
+            case "c":
+                print("\(player2!.playerName) please choose your Characters and name them"
+                    + "\n To choose press 1 for The Fighter, 2 for The Wizard"
+                    + "\n                 3 for The Colossus, 4 for The Dwarf")
+                player2TeamCreation()
             default:
                 print("I don't understand")
+                player1TeamCreation()
             }
         }
     }
@@ -92,15 +106,25 @@ class PlayerManager {
             case "1":
                 print("Choose your Fighter's name")
                 player2?.fighter = createFighterPlayer()
+                print("\(player2!.playerName) when you're done with your 3 characters, press c to continue")
+                player2TeamCreation()
             case "2":
                 print("Choose your Wizard's name")
                 player2?.wizard = createWizardPlayer()
+                print("\(player2!.playerName) when you're done with your 3 characters, press c to continue")
+                player2TeamCreation()
             case "3":
                 print("Choose your Colossus's name")
                 player2?.colossus = createColossusPlayer()
+                print("\(player2!.playerName) when you're done with your 3 characters, press c to continue")
+                player2TeamCreation()
             case "4":
                 print("Choose your Dwarf's name")
                 player2?.dwarf = createDwarfPlayer()
+                print("\(player2!.playerName) when you're done with your 3 characters, press c to continue")
+                player2TeamCreation()
+            case "c":
+                print("next step")
             default:
                 print("I don't understand")
             }
